a2fb238d88979887c33850cf7e20670518faee80
ozcan <28202665+ozcanseker@users.noreply.github.com>
2019-10-03 09:27:38 +0200

    Updates

---
+++ precache-manifest.1f15a8094483ac0b3416dd86cdcec3f6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e1817cde4de70a1bf99a746b9b6097",
+    "url": "/Social-linked-beer/index.html"
+  },
+  {
+    "revision": "620c3dd3626d88280068",
+    "url": "/Social-linked-beer/static/css/main.fc5441f1.chunk.css"
+  },
+  {
+    "revision": "18b1d139d2ff086259c1",
+    "url": "/Social-linked-beer/static/js/2.d4340b1e.chunk.js"
+  },
+  {
+    "revision": "620c3dd3626d88280068",
+    "url": "/Social-linked-beer/static/js/main.e66d1244.chunk.js"
+  },
+  {
+    "revision": "7a462a8689be36d1daba",
+    "url": "/Social-linked-beer/static/js/runtime-main.52c802a6.js"
+  },
+  {
+    "revision": "4dafcfef04c3e2cc13a1c430d271955e",
+    "url": "/Social-linked-beer/static/media/Knipsel.4dafcfef.png"
+  },
+  {
+    "revision": "1a4563f856e72bb4f1f3730862d16a4d",
+    "url": "/Social-linked-beer/static/media/beer.1a4563f8.gif"
+  },
+  {
+    "revision": "6dde892b37a63264fa13a1e097e5d4b0",
+    "url": "/Social-linked-beer/static/media/beerpouring.6dde892b.gif"
+  },
+  {
+    "revision": "bc392ac49495afac8a7f552df2b42478",
+    "url": "/Social-linked-beer/static/media/logo.bc392ac4.png"
+  },
+  {
+    "revision": "b6681619dcc616623083589aa826d2be",
+    "url": "/Social-linked-beer/static/media/profilePic.b6681619.png"
+  }
+]);
